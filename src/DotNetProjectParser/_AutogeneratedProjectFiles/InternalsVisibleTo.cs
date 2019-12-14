bf72b43d9881a476fd2333f6bdd7a72097cec57f
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-12-14 14:44:14 +0100

    Add SNK

--- src/DotNetProjectParser/_AutogeneratedProjectFiles/InternalsVisibleTo.cs
+++ src/DotNetProjectParser/_AutogeneratedProjectFiles/InternalsVisibleTo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("DotNetProjectParser.Tests")]
+[assembly:InternalsVisibleTo("DotNetProjectParser.Tests, " +
+                             "PublicKey=002400000480000094000000060200000024000052534131000400000100010071d7b7b3cffaadb61cb9e81801d67650867692f9788583d6ec3675002077caf51ef0ddebb42c402fd6649a2d736c9703206229d1b4e935dca537cd1448c814936c04c566ba6e499342855f722e2ed5f35099756dbc06a2b9ef53b8ff714234bef53cbd0c132adb303201ce4ce5736f7a69ad0fc27c1c0d8668a5e6cb07bbc1b8")]
