f8719599ee313dc669cb56ce188633a0e128aa86
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-12-16 12:54:11 +0100

    Add test for class library net framework project

--- tests/DotNetProjectParser.Tests/NetFrameworkProjectParserTests.cs
+++ tests/DotNetProjectParser.Tests/NetFrameworkProjectParserTests.cs
@@ -8,7 +8,7 @@ namespace DotNetProjectParser.Tests
     public class NetFrameworkProjectParserTests
     {
         [Test]
-        public void TestProjectLoad_PropertiesReadOk()
+        public void TestProjectLoad_Console_PropertiesReadOk()
         {
             var fileInfo = TestUtils.GetSampleProject(@"DotNetProjectParser.SampleProjects.NetFramework.Console.csproj");
 
@@ -41,6 +41,36 @@ namespace DotNetProjectParser.Tests
                 CopyToOutputDirectory = "Always",
                 Project = project
             });
+        }
+
+
+        [Test]
+        public void TestProjectLoad_Library_PropertiesReadOk()
+        {
+            var fileInfo = TestUtils.GetSampleProject(@"DotNetProjectParser.SampleProjects.NetFramework.Library.csproj");
+
+            var project = DotNetProjectParser.ProjectFactory.GetProject(fileInfo);
+
+            project.Should().NotBeNull();
+            project.ProjectXml.Should().NotBeNull();
+            project.Name.Should().Be("DotNetProjectParser.SampleProjects.NetFramework.Library.csproj");
+            project.FullPath.Should().Be(fileInfo.FullName);
+            project.DirectoryPath.Should().Be(fileInfo.Directory.FullName);
+            project.AssemblyName.Should().Be("DotNetProjectParser.SampleProjects.NetFramework.Library");
+            project.OutputType.Should().Be("Library");
+            project.TargetExtension.Should().Be(".dll");
+            project.TargetFramework.Should().Be("v4.7");
+
+            project.Items.Should().ContainEquivalentOf(new ProjectItem()
+            {
+                Include = "Class1.cs",
+                ResolvedIncludePath = Path.Combine(fileInfo.Directory.FullName, "Class1.cs"),
+                ItemType = "Compile",
+                CopyToOutputDirectory = null,
+                Project = project
+            });
+
+            
         }
 
     }
